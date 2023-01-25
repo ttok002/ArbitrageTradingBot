c37fcd2ed9f8f0fa6c8ee206a6d9590757085810
Peter Pisarz <peter.pisarz@gmail.com>
2023-01-25 15:09:12 -0600

    Initial Commit

---
+++ hardhat.config.js
@@ -0,0 +1,19 @@
+require("@nomicfoundation/hardhat-toolbox")
+require("dotenv").config()
+const privateKeys = process.env.PRIVATE_KEYS || ""
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.9",
+  networks: {
+    hardhat: {
+      forking: {
+        url: `https://eth-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+      }
+    }
+    mainnet: {
+      url: `https://eth-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts: [process.env.PRIVATE_KEY],
+    }
+  }
+};
